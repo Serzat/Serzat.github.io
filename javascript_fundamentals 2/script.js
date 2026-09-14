4937d7bbc58f842a728dceac218e40aab5927b45
Serzat <serzat300@outlook.com>
2026-09-14 11:22:39 +0200

    ..

---
+++ javascript_fundamentals 2/script.js
@@ -0,0 +1,34 @@
+const producten = [
+    {
+        naam: "Laptop",
+        prijs: 800,
+        populariteit: 90
+    },
+    {
+        naam: "Muis",
+        prijs: 30,
+        populariteit: 70
+    },
+    {
+        naam: "Toetsenbord",
+        prijs: 60,
+        populariteit: 80
+    },
+    {
+        naam: "Monitor",
+        prijs: 250,
+        populariteit: 85
+    },
+    {
+        naam: "Headset",
+        prijs: 75,
+        populariteit: 65
+    },
+    {
+        naam: "Webcam",
+        prijs: 50,
+        populariteit: 60
+    }
+];
+
+console.log(producten);
